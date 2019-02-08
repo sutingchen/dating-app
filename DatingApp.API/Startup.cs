7890591d1b721fafc7d3d6fe8c8785bf427730a1
Chen Su Ting <wendy10935@hotmail.com>
2019-02-08 20:32:24 +0800

    Startup.cs AddCors() and UseCors()

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -21,7 +21,8 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("Default")));  
+            services.AddDbContext<DataContext>(options => options.UseSqlite(Configuration.GetConnectionString("Default"))); 
+            services.AddCors(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,7 @@ namespace DatingApp.API
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
