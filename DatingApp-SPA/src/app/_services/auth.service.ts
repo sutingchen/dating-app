cce16dad8b579070d2886e28f860635c97398933
Chen Su Ting <wendy10935@hotmail.com>
2019-04-03 20:01:40 +0800

    Add the main photo to the Nav bar

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,9 @@ export class AuthService {
   baseUrl = environment.apiUrl + 'auth/';
   decodedToken: any;
   jwtHelper = new JwtHelperService();
+  currentUser: User;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'login', model).pipe(
@@ -20,7 +22,9 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          localStorage.setItem('user', JSON.stringify(user.user));
           this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          this.currentUser = user.user;
           console.log(this.decodedToken);
         }
       })
@@ -35,5 +39,4 @@ export class AuthService {
     const token = localStorage.getItem('token');
     return !this.jwtHelper.isTokenExpired(token);
   }
-
 }
--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -41,4 +41,8 @@ export class MemberEditComponent implements OnInit {
     });
   }
 
+  updateMainPhoto(photoUrl) {
+    this.user.photoUrl = photoUrl;
+  }
+
 }
