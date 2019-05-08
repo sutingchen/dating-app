b1d1b7af2f67a29a75ce0aa3452c9d596658a3c0
Chen Su Ting <wendy10935@hotmail.com>
2019-05-08 16:14:55 +0800

    Updated the Register method in the API

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -38,15 +38,14 @@ namespace DatingApp.API.Controllers
                 return BadRequest("Username already exists.");
             }
 
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
+            var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
+
             // return CreatedAtRoute
-            return StatusCode(201);
+            return CreatedAtRoute("GetUser", new { controller = "Users", id = createdUser.Id }, userToReturn);
         }
 
         [HttpPost("login")]
