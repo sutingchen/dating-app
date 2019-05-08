58505b14e6220de99dba38dddcba812122646cc7
Chen Su Ting <wendy10935@hotmail.com>
2019-05-08 23:10:37 +0800

    Complete the register implementation

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,10 +16,11 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
-  constructor(private authService: AuthService,
+  constructor(private authService: AuthService, private router: Router,
     private alertify: AlertifyService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -50,6 +53,19 @@ export class RegisterComponent implements OnInit {
     // }, error => {
     //   this.alertify.error(error);
     // });
+
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(() => {
+        this.alertify.success('Registration successful');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe(() => {
+          this.router.navigate(['/members']);
+        });
+      });
+    }
     console.log(this.registerForm.value);
   }
 
